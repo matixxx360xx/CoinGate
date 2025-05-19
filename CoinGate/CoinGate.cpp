54edd6e7b146526891914fe334b10628ba185c50
MSI\mati <matimichalski01@gmail.com>
2025-05-20 00:45:13 +0200

    Poprawiono ruszanie sie po planszy w kodzie i dodano zebranie klucza i otworzenie drzwi

--- CoinGate/CoinGate.cpp
+++ CoinGate/CoinGate.cpp
@@ -5,64 +5,86 @@ using namespace std;
 
 class Player {  
 public:  
- int x;  
- int y;  
- string move;  
- 
+int x;  
+int y;  
+int C = 0;
+int D = 0;
+string move;  
 
- Player(int dx, int dy) {  
-     x = dx;  
-     y = dy;  
-     
- }  
+static const int rows = 3;
+static const int cols = 4;
+char tablica[rows][cols] = {
+    {'*', '*', '*', '*'},
+    {'*', '*', '*', 'C'},
+    {'*', 'D', '*', '*'}
+};
 
- void ClearConsole() {
-     system("cls");
- }
- void Map() {
-     ClearConsole();
-     static const int rows = 3;
-     static const int cols = 4;
-     char tablica[rows][cols] = {
-         {'*', '*', '*', '*'},
-         {'*', '*', '*', '*'},
-         {'*', '*', '*', '*'}
-     };
-     tablica[y][x] = 'U';
-     for (int i = 0; i < rows; i++) {
-         for (int j = 0; j < cols; j++) {
-             cout << tablica[i][j] << " ";
-         }
-         cout << endl;  
-     }
-     cout << "pozycja x: " << x << ", pozycja y: " << y << endl;
- }
+Player(int dx, int dy) {  
+    x = dx;  
+    y = dy;  
+    
+}  
 
- void Position() {  
-     if (move == "w") {  
-         y -= 1;  
-     } else if (move == "s") {  
-         y += 1;  
-     } else if (move == "a") {  
-         x -= 1;  
-     } else if (move == "d") {  
-         x += 1;  
-     }  
- }  
+void ClearConsole() {
+    system("cls");
+}
+
+void Map() {
+    ClearConsole();
+  
+    
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (i == y && j == x) {
+                cout << 'U' << " ";
+            } else{
+                cout << tablica[i][j] << " ";
+        }
+        }
+        cout << endl;  
+    }
+    if (x == 3 && y == 1 && tablica[1][3] == 'C') {
+        C += 1;
+        tablica[1][3] = '*'; 
+        cout << "Zebrane C: " << "Zebrano klucz!" << endl;
+    }
+    if (x == 1 && y == 2 && C ==1 ) {
+        cout << "Gratulacje otworzyles drzwi!" << endl;
+    }
+    else {
+        cout << "Zdobadz klucz!" << endl;
+    }
+    
+    cout << "pozycja x: " << x << ", pozycja y: " << y << endl;
+    
+}
+
+void Position() {  
+    
+    if (move == "w") {  
+        y -= 1;  
+    } else if (move == "s") {  
+        y += 1;  
+    } else if (move == "a") {  
+        x -= 1;  
+    } else if (move == "d") {  
+        x += 1;  
+    }  
+}  
 };  
 
 int main()  
 {  
- Player player(0, 0);  
+Player player(0, 0);  
 
 
- while (true) {  
-     player.Position();
-     player.Map();
-     
-     cout << "Ruch (w,a,s,d)\n";  
-     cin >> player.move;  
-     
-     
+while (true) {  
+    player.Position();
+    player.Map();
+    
+    cout << "Ruch (w,a,s,d)\n";  
+    cin >> player.move;  
+
+
  }  
 }
