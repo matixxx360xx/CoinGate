f7d0974824f6d655a9ee6d67d7be8e83294c1fcc
MSI\mati <matimichalski01@gmail.com>
2025-05-19 20:28:39 +0200

    Dodanie ruchu gracza i metody czyszczenia konsoli

--- CoinGate/CoinGate.cpp
+++ CoinGate/CoinGate.cpp
@@ -1,42 +1,68 @@
-﻿#include <iostream>
-#include <string>
-using namespace std;
+﻿#include <iostream>  
+#include <string>  
+#include <windows.h>
+using namespace std;  
 
-class Player {
-public:
-  int x;
-  int y;
-  string move;
+class Player {  
+public:  
+ int x;  
+ int y;  
+ string move;  
+ 
 
-  Player(int dx, int dy) {
-      x = dx;
-      y = dy;
-      cout << "Tworzenie gracza na pozycji x: " << dx << ", y: " << dy << endl;
-  }
-  
-  void Position() {
-      if (move == "w") {
-          y += 1;
-      }else if(move == "s") {
-          y -= 1;
-      }
-      else if (move == "a") {
-          x -= 1;
-      }
-      else if (move == "d") {
-          x += 1;
-      }
-      cout << "pozycja x: " << x << ", pozycja y: " << y << endl;
-  }
-};
+ Player(int dx, int dy) {  
+     x = dx;  
+     y = dy;  
+     
+ }  
 
-int main()
-{
-  Player player(0, 0); 
+ void ClearConsole() {
+     system("cls");
+ }
+ void Map() {
+     ClearConsole();
+     static const int rows = 3;
+     static const int cols = 4;
+     char tablica[rows][cols] = {
+         {'*', '*', '*', '*'},
+         {'*', '*', '*', '*'},
+         {'*', '*', '*', '*'}
+     };
+     tablica[y][x] = 'U';
+     for (int i = 0; i < rows; i++) {
+         for (int j = 0; j < cols; j++) {
+             cout << tablica[i][j] << " ";
+         }
+         cout << endl;  
+     }
+     cout << "pozycja x: " << x << ", pozycja y: " << y << endl;
+ }
 
-  while (true) {
-      cout << "Ruch (w,a,s,d)\n";
-      cin >> player.move;
-      player.Position();
-  }
+ void Position() {  
+     if (move == "w") {  
+         y -= 1;  
+     } else if (move == "s") {  
+         y += 1;  
+     } else if (move == "a") {  
+         x -= 1;  
+     } else if (move == "d") {  
+         x += 1;  
+     }  
+ }  
+};  
+
+int main()  
+{  
+ Player player(0, 0);  
+
+
+ while (true) {  
+     player.Position();
+     player.Map();
+     
+     cout << "Ruch (w,a,s,d)\n";  
+     cin >> player.move;  
+     
+     
+ }  
 }
